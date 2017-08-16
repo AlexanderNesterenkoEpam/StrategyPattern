6c561376d75eceb6803c69fa0e388492d82ec905
Aliaksandr Nestsiarenka1 <Aliaksandr_Nestsiarenka1@epam.com>
2017-08-16 20:39:10 +0300

    first commit

---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			JavaDeveloper Alex = new JavaDeveloper();
+			Alex.activity = new Coding();
+			Alex.DoActivity();
+
+			Alex.activity = new Sleeping();
+			Alex.DoActivity();
+
+		}
+	}
+}
